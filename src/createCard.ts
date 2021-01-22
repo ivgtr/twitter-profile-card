2b5df56411fe2633166b2ee4194430cde6f7746f
ivgtr <gear.1112t.o@gmail.com>
2021-01-22 13:10:41 +0900

    :ambulance: fix: インデントの修正

--- src/createCard.ts
+++ src/createCard.ts
@@ -60,14 +60,14 @@ export async function createCard(tweetData: FullUser, color: colors) {
   await browser.close()
 
   return `
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="${width}"
-    height="${height}"
-    viewport="0 0 ${width} ${height}"
-    fill="none"
-  >
-    <image href="data:image/jpeg;base64,${buffer}" x="0" y="0" width="100%" height="100%"/>
-  </svg>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="${width}"
+      height="${height}"
+      viewport="0 0 ${width} ${height}"
+      fill="none"
+    >
+      <image href="data:image/jpeg;base64,${buffer}" x="0" y="0" width="100%" height="100%"/>
+    </svg>
   `
 }
